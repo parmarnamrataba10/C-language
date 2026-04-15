64673d9a50ae370bc8e1ec14a18e29e89cd3f20b
parmarnamrataba10 <parmarnamrataba0@gmail.com>
2026-04-15 13:12:52 +0530

    start

---
+++ 01.intro/name.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+
+int main(){
+
+    printf("\nName:namrataba");
+    printf("\nAge:18 ");
+    printf("\ncollage:ssccm");
+
+    return 0;
+
+}
---
+++ 01.intro/pattern.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+
+int main(){
+
+    printf("-----");
+    printf("\n|\t|");
+    printf("\nR\t|");
+    printf("\nN\t|");
+    printf("\nw\t|");
+    printf("\n|\t|");
+    printf("\n-----");
+    return 0;
+
+}
